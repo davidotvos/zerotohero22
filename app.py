fda80440d600d7304ca43129d6a0f253d2c278fb
davidotvos <davidotvos@wtf.com>
2022-08-12 20:13:05 +0200

    cashier adatbázishoz adás, POST,GET request test

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, request, jsonify
-import requests
+from flask import Flask, render_template, request, jsonify, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# adatbázis inicializálás
+db = SQLAlchemy(app)
+
+class Cashier(db.Model):
+    id = db.Column(db.String(30), primary_key=True)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(200), nullable=False)
+
+    def repr_(self):
+        return'<Name%r>' % self.id
+    
 
 
 @app.route('/', methods=['GET'])
@@ -9,14 +26,32 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/cashier', methods=['POST'])
+@app.route('/cashier', methods=['POST', 'GET'])
 def cashier():
-    data = request.get_json()
-    
-    first_name = data['firstName']
-    last_name = data['lastName']
 
-    return jsonify({'result' : 'Success!', 'firstName' : first_name, 'lastName' : last_name})
+    if request.method == 'POST':
+            
+        data = request.get_json()
+        
+        firstName = data['firstName']
+        lastName = data['lastName']
+
+        new_cashier = Cashier(id = '2', first_name = firstName, last_name = lastName)
+
+        #return jsonify({'result' : 'Success!', 'firstName' : first_name, 'lastName' : last_name})
+        
+        #adatbázishoz adás
+        try:
+            db.session.add(new_cashier)
+            db.session.commit()
+            return redirect('/cashier')
+        except:
+            return 'Hiba az adatbázishoz adásnál!'
+
+    else:
+        cashiers = Cashier.query.order_by(Cashier.id)
+        return render_template('cashier.html', cashiers=cashiers)
+
 
 
 @app.route('/sale', methods=['POST'])
