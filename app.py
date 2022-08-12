639278c4942bc2fc6669d9977bf750e24e5be371
David <davidotvos365@gmail.com>
2022-08-12 15:37:33 +0200

    cashier post request commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/cashier', methods=['POST'])
+def cashier():
+    data = request.get_json()
+    first_name = data['firstName']
+    last_name = data['lastName']
+
+    return jsonify({'result' : 'Success!', 'firstName' : first_name, 'lastName' : last_name})
+
 
 
 if __name__ == '__main__':
